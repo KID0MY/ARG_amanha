e22c2042af47afb19b3a312e3131a85636bf5a4b
KID0MY <lukazbujes@gmail.com>
2023-04-20 19:16:10 -0300

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("August 31, 2022 15:25:00").getTime();
+var countDownDate = new Date("August 31, 2023 15:25:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
