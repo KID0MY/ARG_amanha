0308e1c8f8b078fd18a208568cff09ef8a32965a
Lucas Triboli <57596595+KID0MY@users.noreply.github.com>
2023-08-30 10:41:04 -0300

    Add files via upload

--- timer.js
+++ timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("August 31, 2023 15:25:00").getTime();
+var countDownDate = new Date("August 30, 2023 12:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
